0707618acb1de2c7b2ca46c5dc165730e75eb51b
Buddy <buddy@clawd.bot>
2026-02-24 20:47:47 +0000

    Update version strings in Settings and logo toast to 1.0.3

--- app/src/main/java/com/proteinscannerandroid/MainActivity.kt
+++ app/src/main/java/com/proteinscannerandroid/MainActivity.kt
@@ -114,7 +114,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.logoButton.setOnClickListener {
             // Could add easter egg or info screen here
-            Toast.makeText(this, "Protein Quality Scanner v1.0", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Protein Quality Scanner v1.0.3", Toast.LENGTH_SHORT).show()
         }
     }
 
